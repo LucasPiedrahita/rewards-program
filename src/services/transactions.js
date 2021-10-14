8d2650579b6594ac97466b1666fcaa5018cf5f64
LucasPiedrahita <Lucas.Piedrahita96@gmail.com>
2021-10-14 12:54:51 -0400

    add doc strings

--- src/services/transactions.js
+++ src/services/transactions.js
@@ -2,6 +2,11 @@ import axios from 'axios'
 
 const baseUrl = 'http://localhost:3001/transactions'
 
+/**
+ * Function that fetches transaction data asynchronously
+ * @author   Lucas Piedrahita
+ * @return   {Array.<{_id: String, amount: Number, customerId: String, date: String}>}  Array of transactions
+ */
 const getAll = async () => {
   const request = axios.get(baseUrl)
   const response = await request
--- src/utils/getMonthNameByNumber.js
+++ src/utils/getMonthNameByNumber.js
@@ -1,3 +1,9 @@
+/**
+ * Function that converts a number to full month name
+ * @author   Lucas Piedrahita
+ * @param    {Number} monthNumber    Zero-based number of the month, where January is 0
+ * @return   {String}                Full name of month
+ */
 const getMonthNameByNumber = (monthNumber) => {
   const months = [
     'January',
