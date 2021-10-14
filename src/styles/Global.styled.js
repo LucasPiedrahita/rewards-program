d4f903c405e86d969992c01a0322a5fe35b07fad
LucasPiedrahita <Lucas.Piedrahita96@gmail.com>
2021-10-14 16:51:27 -0400

    update global styles

--- src/styles/Global.styled.js
+++ src/styles/Global.styled.js
@@ -1,10 +1,6 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
-  * {
-    box-sizing: border-box
-  }
-
   h1, footer {
     padding: 2rem 0;
     color: ${({ theme }) => theme.colors.white};
